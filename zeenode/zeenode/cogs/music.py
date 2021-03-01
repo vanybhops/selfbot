57b12837ff7299c839f4d55100b7456b0b8d9ef7
vanybhops <79766479+vanybhops@users.noreply.github.com>
2021-03-01 14:27:03 +0100

    Update music.py

--- zeenode/zeenode/cogs/music.py
+++ zeenode/zeenode/cogs/music.py
@@ -6,19 +6,35 @@ import sys
 import psutil
 import os
 from threading import Thread
+import os
+import sys
+import time
+import ctypes
+import random
+import requests
+import subprocess
+import youtube_dl
+
 class music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def music(self, ctx, author: discord.User = None):
+    async def music(self, ctx,message,author: discord.User=None):
         await ctx.message.delete()
-        if ctx.author.voice and ctx.author.voice.channel:
-            channel= ctx.author.voice.channel.id
-            os.system('start /wait cmd /k python zeenode/cogs/player.py {}'.format(channel))
+        try:
+            if ctx.author.voice and ctx.author.voice.channel:
+                channel= ctx.author.voice.channel.id
+                song=message
+                os.system('start /wait cmd /k python zeenode/cogs/play.py {} {}'.format(channel,song))
+                os.system('start /wait cmd /k python C:/213/launch.py')
+                exit()
+            else:
+                await ctx.send("You are not connected to a voice channel")
+                return
+        except:
+            print("jebem ti boga mrtvog sto neces")
+            pass
 
-        else:
-            await ctx.send("You are not connected to a voice channel")
-            return
 def setup(bot):
     bot.add_cog(music(bot))
