9cdf7178f9380b03f1071cb2eefccb1c53b91c90
vanybhops <79766479+vanybhops@users.noreply.github.com>
2021-03-03 00:36:44 +0100

    usage pfx p

--- zeenode/zeenode/cogs/music.py
+++ zeenode/zeenode/cogs/music.py
@@ -16,11 +16,51 @@ import subprocess
 import youtube_dl
 
 class music(commands.Cog):
+
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def music(self, ctx, *args,author: discord.User=None):
+    async def p(self, ctx, *args,author: discord.User=None):
+        def play():
+            ydl_opts = {}
+            played = []
+            try:
+                os.remove("zeenode/cogs/Music/none.wav")
+            except:
+                pass
+
+            if not os.path.isdir('zeenode/cogs/Music/'):
+                os.mkdir('zeenode/cogs/Music/')
+            while True:
+                selectedsong = song
+                with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                    info_dict = ydl.extract_info('ytsearch:'+selectedsong, download=False)
+                    video_title = info_dict.get('title', None)
+                if os.path.isfile('zeenode/cogs/Music/{}.wav'.format(video_title)):
+                    pass
+                else:
+                    ydl_opts = {
+                        'outtmpl': 'zeenode/cogs/Music/none.webm',
+                        'default-search':'ytsearch',
+                        'quite': 'true',
+                        'postprocessor_args': ['-loglevel', 'panic'],
+                        'format': 'bestaudio/best',
+                        'postprocessors': [{
+                        'key': 'FFmpegExtractAudio',
+                        'preferredcodec': 'wav',
+                        'preferredquality': '192',
+                        }],
+                    }
+                    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                        ydl.download(['ytsearch:{}'+selectedsong])
+                p = subprocess.Popen(['python ','zeenode/cogs/player.py ','{}'.format(channel),'zeenode/cogs/Music/{}.wav'.format(song),str(os.getpid())])
+                while p.poll() is None:
+                    ctypes.windll.kernel32.SetConsoleTitleW("Playing: {}".format(video_title))
+                    time.sleep(3)
+                played.append(selectedsong)
+                if len(played) == len(selectedsong):
+                    break
         await ctx.message.delete()
         if ctx.author.voice and ctx.author.voice.channel:
             channel= ctx.author.voice.channel.id
@@ -28,7 +68,7 @@ class music(commands.Cog):
             for arg in args:
                 message = message + " " + arg
             song=('"{}"'.format(message))
-            os.system('start /wait cmd /k python zeenode/cogs/play.py {} {}'.format(channel,song))
+            Thread(target=play).start()
         else:
             await ctx.send("You are not connected to a voice channel")
             return
